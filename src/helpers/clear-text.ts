56afb623d01e7b99d3763ed0b229f4cea5d7b8b8
João Genari <victor.olr@hotmail.com>
2024-07-23 22:17:08 -0300

    feat: new function to clear strings

---
+++ src/helpers/clear-text.ts
@@ -0,0 +1,11 @@
+const removeAccentsAndSpecialChars = (text: string) => {
+  return text.normalize("NFD").replace(/[^\w\s.]/g, "").replace(/[\u0307]/g, "")
+}
+
+export const cleanString = (text: string) => {
+  text = text.toLowerCase()
+  text = removeAccentsAndSpecialChars(text)
+  text = text.replace(/[^\w\s.]/g, '')
+
+  return text
+}
