9b373e207167662d460c86b963df817d6d72d6c4
João Genari <victor.olr@hotmail.com>
2024-10-01 20:21:54 -0300

    refactor: adjusting create product fields

--- src/validators/products/products-validators.ts
+++ src/validators/products/products-validators.ts
@@ -11,9 +11,9 @@ export const productParamsSchema = z.object({
 export const createProductBodySchema = z.object({
 	name: z.string().min(1, 'Produto inválido'),
 	description: z.string().min(1, 'Descrição inválida'),
-	quantity: z.number().min(1, 'Quantidade inválida'),
-	price: z.number().min(1, 'Valor inválido'),
-	category_id: z.number().min(1, 'Categoria inválida'),
+	quantity: z.coerce.number().min(1, 'Quantidade inválida'),
+	price: z.coerce.number().min(1, 'Valor inválido'),
+	category_id: z.coerce.number().min(1, 'Categoria inválida'),
 })
 
 export const updateProductBodySchema = z.object({
